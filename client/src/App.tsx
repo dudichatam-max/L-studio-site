6b877c9e803b59d603ca2d6803bd8468668420f1
dudichatam-max <dudichatam@gmail.com>
2026-09-22 15:38:12 +0300

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { Route, Router as WouterRouter, Switch } from "wouter";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
+import Home from "./pages/Home";
+import Privacy from "./pages/Privacy";
+import NotFound from "./pages/NotFound";
+
+function Router() {
+  const base = window.location.pathname.startsWith("/L-studio-site") ? "/L-studio-site" : "";
+  return <WouterRouter base={base}><Switch><Route path="/" component={Home} /><Route path="/privacy" component={Privacy} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch></WouterRouter>;
+}
+
+export default function App() {
+  return <ErrorBoundary><LanguageProvider><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster /><Router /></TooltipProvider></ThemeProvider></LanguageProvider></ErrorBoundary>;
+}
